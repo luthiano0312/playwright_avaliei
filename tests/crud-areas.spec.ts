326deea1b7916195db9badc95803c73d85acfc01
Luthiano Miguel <luthianomiguel@gmail.com>
2026-05-28 21:52:32 -0300

    feat: arquivo de teste do crud de areas

---
+++ tests/crud-areas.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login-page';
+import { DashboardPage } from '../pages/dashboard-page';
+import { AreasPage } from '../pages/areas-page';
+
+
+test('crudAreas', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+    const areasPage = new AreasPage(page);
+
+    const email = "e2e-super-teacher-26@example.com";
+    const password = "password";
+
+    const uniqueId = Date.now();
+    const areaName = `Tecnologia E2E - ${uniqueId}`;
+    const updatedAreaName = `Tecnologia E2E - ${uniqueId} (Editada)`;
+
+    await test.step("loginAndGoToAreasPage", async () => {
+        await loginPage.goToLoginPage();
+        await loginPage.login(email, password);
+        await loginPage.otpCodePage();
+
+        await dashboardPage.userIsLogged();
+        await dashboardPage.goToAreas();
+    });
+
+    await test.step("createArea", async () => {
+        await areasPage.createArea(areaName);
+    });
+
+    await test.step("updateArea", async () => {
+        await areasPage.updateArea(areaName, updatedAreaName);
+    });
+    
+    await test.step("deleteArea", async () => {
+        await areasPage.deleteArea(updatedAreaName);
+    });
+});
